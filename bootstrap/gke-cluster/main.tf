5c76599715d6ee1dab39d967877e762ae8389176
vedantthapa <thapavedant@gmail.com>
2024-05-02 15:03:40 -0300

    feat: add asm module

--- bootstrap/gke-cluster/main.tf
+++ bootstrap/gke-cluster/main.tf
@@ -1,5 +1,10 @@
 data "google_client_config" "default" {}
 
+data "google_project" "project" {
+  project_id = var.project_id
+}
+
+
 resource "google_artifact_registry_repository" "ph_backstage_repo" {
   location      = local.region
   repository_id = "ph-backstage"
@@ -42,6 +47,7 @@ module "gke" {
   grant_registry_access      = true
   release_channel            = "REGULAR"
   deletion_protection        = false
+  cluster_resource_labels    = { "mesh_id" : "proj-${data.google_project.project.number}" }
 }
 
 resource "google_service_account" "phac-backstage-kcc-sa" {
@@ -133,4 +139,15 @@ module "config_sync" {
   sync_repo     = var.config_sync_repo
   sync_branch   = var.config_sync_branch
   policy_dir    = "root-sync/overlays/${var.config_sync_kustomize_overlay}"
+}
+
+module "asm" {
+  source                    = "terraform-google-modules/kubernetes-engine/google//modules/asm"
+  project_id                = var.project_id
+  cluster_name              = module.gke.name
+  cluster_location          = module.gke.location
+  enable_cni                = true
+  mesh_management           = "MANAGEMENT_AUTOMATIC"
+  enable_fleet_registration = true
+  enable_mesh_feature       = true
 }
