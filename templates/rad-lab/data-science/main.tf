1eab8f02eef11f4b8a031a4ae8e6155b6f116db5
rajan <ssubedir@gmail.com>
2024-05-16 14:42:32 -0400

    fmt

--- templates/rad-lab/data-science/main.tf
+++ templates/rad-lab/data-science/main.tf
@@ -16,22 +16,22 @@
 
 locals {
   random_id = var.deployment_id != null ? var.deployment_id : random_id.default.0.hex
-  project   = (var.create_project
-  ? try(module.project_radlab_ds_analytics.0, null)
-  : try(data.google_project.existing_project.0, null)
+  project = (var.create_project
+    ? try(module.project_radlab_ds_analytics.0, null)
+    : try(data.google_project.existing_project.0, null)
   )
   region = join("-", [split("-", var.zone)[0], split("-", var.zone)[1]])
 
   network = (
-  var.create_network
-  ? try(module.vpc_ai_notebook.0.network.network, null)
-  : try(data.google_compute_network.default.0, null)
+    var.create_network
+    ? try(module.vpc_ai_notebook.0.network.network, null)
+    : try(data.google_compute_network.default.0, null)
   )
 
   subnet = (
-  var.create_network
-  ? try(module.vpc_ai_notebook.0.subnets["${local.region}/${var.subnet_name}"], null)
-  : try(data.google_compute_subnetwork.default.0, null)
+    var.create_network
+    ? try(module.vpc_ai_notebook.0.subnets["${local.region}/${var.subnet_name}"], null)
+    : try(data.google_compute_subnetwork.default.0, null)
   )
 
   notebook_sa_project_roles = [
@@ -49,7 +49,7 @@ locals {
     "notebooks.googleapis.com",
     "bigquerystorage.googleapis.com"
   ]
-  project_services = var.enable_services ? (var.billing_budget_pubsub_topic ? distinct(concat(local.default_apis,["pubsub.googleapis.com"])) : local.default_apis) : []
+  project_services = var.enable_services ? (var.billing_budget_pubsub_topic ? distinct(concat(local.default_apis, ["pubsub.googleapis.com"])) : local.default_apis) : []
 }
 
 resource "random_id" "default" {
