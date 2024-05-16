0695b81d620bc3d3a7637e6aa0e7f809edf487d4
Sean Poulter <sean.poulter@focisolutions.com>
2024-05-16 10:23:10 -0400

    Add the Backstage template for the Rad Lab Data Science module
    Co-authored-by: ssubedir <ssubedir@users.noreply.github.com>

---
+++ backstage/packages/backend/src/plugins/scaffolder/templates/rad-lab-data-science-create.test.ts
@@ -0,0 +1,54 @@
+import { createMockDirectory } from '@backstage/backend-test-utils';
+import { fetchTemplateActionHandler } from './__testUtils__/fetchTemplateActionHandler';
+
+describe('rad-lab-data-science-create: fetch:template', () => {
+  const mockDir = createMockDirectory();
+
+  afterEach(() => {
+    mockDir.remove();
+  });
+
+  test('The fetch:template action should render the expected changes for the Pull Request', async () => {
+    await fetchTemplateActionHandler({
+      name: 'rad-lab-data-science-create',
+      values: {
+        requestId: '<uuid>',
+        rootFolderId: '<root-folder-id>',
+        projectName: '<project-name>',
+        projectId: '<project-id>',
+        owners: [
+          { email: 'jane.doe@gcp.hc-sc.gc.ca' },
+          { email: 'john.doe@gcp.hc-sc.gc.ca' },
+        ],
+        editors: [
+          { email: 'samantha.jones@gcp.hc-sc.gc.ca' },
+          { email: 'alex.mcdonald@gcp.hc-sc.gc.ca' },
+          { email: 'john.campbell@gcp.hc-sc.gc.ca' },
+        ],
+      },
+      mockDir,
+    });
+
+    expect(mockDir.content({ path: 'workspace' })).toMatchInlineSnapshot(`
+      {
+        "claim.yaml": "---
+      apiVersion: data-science-portal.phac-aspc.gc.ca/v1alpha1
+      kind: RadLabDataScienceClaim
+      metadata:
+        name: <project-name>-<uuid>
+      spec:
+        rootFolderId: '<root-folder-id>'
+        projectName: <project-name>
+        projectId: <project-id>
+        projectEditors:
+          - user:jane.doe@gcp.hc-sc.gc.ca
+          - user:john.doe@gcp.hc-sc.gc.ca
+        projectViewers:
+          - user:samantha.jones@gcp.hc-sc.gc.ca
+          - user:alex.mcdonald@gcp.hc-sc.gc.ca
+          - user:john.campbell@gcp.hc-sc.gc.ca
+      ",
+      }
+    `);
+  });
+});
