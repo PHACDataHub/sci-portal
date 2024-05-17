a9c0163a59c7cfd7a9648657a707f77f1d2a9390
Sean Poulter <sean.poulter@focisolutions.com>
2024-05-17 12:19:57 -0400

    Refactor tests

--- backstage/packages/backend/src/plugins/scaffolder/__testUtils__/getContextActionHandler.ts
+++ backstage/packages/backend/src/plugins/scaffolder/__testUtils__/getContextActionHandler.ts
@@ -1,36 +1,47 @@
 import { MockDirectory, mockServices } from '@backstage/backend-test-utils';
 import { createProvisionTemplateAction } from '../actions/provisioner';
 import { createMockActionContext } from '@backstage/plugin-scaffolder-node-test-utils';
+import { RootConfigService } from '@backstage/backend-plugin-api';
+import { projectParameters } from '../templates/project-create.test';
+import { ActionContext } from '@backstage/plugin-scaffolder-node';
+import { JsonObject } from '@backstage/types';
+
+const rootConfig = mockServices.rootConfig({
+  data: {
+    backend: {
+      plugins: {
+        provisioner: {
+          repo: {
+            owner: '<repo-owner>',
+            name: '<repo-name>',
+          },
+          templateDir: './templates',
+        },
+      },
+    },
+  },
+});
 
 export const getContextActionHandler = async ({
   template: { namespace = 'default', name, title },
+  config = rootConfig,
   parameters,
   mockDir,
 }: {
   template: { namespace?: string; name: string; title?: string };
-  parameters: any;
+  parameters?: any;
+  config?: RootConfigService;
   mockDir: MockDirectory;
-}) => {
-  const config = mockServices.rootConfig({
-    data: {
-      backend: {
-        plugins: {
-          provisioner: {
-            repo: {
-              owner: '<repo-owner>',
-              name: '<repo-name>',
-            },
-            templateDir: './templates',
-          },
-        },
-      },
-    },
-  });
+}): Promise<{
+  ctx: ActionContext<JsonObject> & { getOutput: (name: string) => any };
+}> => {
   const action = createProvisionTemplateAction(config);
-
   const ctx = createMockActionContext({
     input: {
-      parameters,
+      parameters: {
+        ...projectParameters,
+        ...parameters,
+      },
     },
     templateInfo: {
       entity: {
@@ -44,7 +55,22 @@ export const getContextActionHandler = async ({
     },
     workspacePath: mockDir.resolve('workspace'),
   });
+
   await action.handler(ctx);
 
-  return { ctx };
+  return {
+    ctx: {
+      ...ctx,
+
+      /**
+       * Returns the value that ctx.output(name) was called with, or undefined.
+       */
+      getOutput(name) {
+        const calls = (ctx.output as jest.Mock).mock.calls.filter(
+          args => args[0] === name,
+        );
+        return calls[calls.length - 1]?.[1];
+      },
+    },
+  };
 };
