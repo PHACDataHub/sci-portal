373baee980cd4d50050a24c0114f464decfdc059
Sean Poulter <98067886+seanpoulter-foci@users.noreply.github.com>
2024-05-22 08:04:06 -0700

    Move the Backstage Cloud Build configuration (#270)

--- bootstrap/gke-cluster/main.tf
+++ bootstrap/gke-cluster/main.tf
@@ -28,7 +28,7 @@ resource "google_cloudbuild_trigger" "data_science_portal_trigger" {
     }
   }
   included_files = ["backstage/**"]
-  filename       = "cloudbuild.yaml"
+  filename       = "backstage/packages/backend/cloudbuild.yaml"
 }
 
 module "gke" {
