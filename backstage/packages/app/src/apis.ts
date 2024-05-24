cc0c003ffcf34384509f356d66332272a9f53763
Sean Poulter <sean.poulter@focisolutions.com>
2024-05-24 09:32:33 -0400

    Remove Cost Insights

--- backstage/packages/app/src/apis.ts
+++ backstage/packages/app/src/apis.ts
@@ -8,19 +8,12 @@ import {
   configApiRef,
   createApiFactory,
 } from '@backstage/core-plugin-api';
-import { costInsightsApiRef } from '@backstage-community/plugin-cost-insights';
-import { CostInsightsClient } from './costInsights/CostInsightsClient';
 
 export const apis: AnyApiFactory[] = [
   createApiFactory({
     api: scmIntegrationsApiRef,
     deps: { configApi: configApiRef },
     factory: ({ configApi }) => ScmIntegrationsApi.fromConfig(configApi),
-  }),
-  createApiFactory({
-    api: costInsightsApiRef,
-    deps: {},
-    factory: () => new CostInsightsClient(),
   }),
 
   ScmAuth.createDefaultApiFactory(),
