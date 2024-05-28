5796c1d96e761373345c95d3c7696d6e6c64d024
Sean Poulter <sean.poulter@focisolutions.com>
2024-05-28 19:48:21 -0400

    Apply code formatting

--- backstage/packages/backend/src/plugins/scaffolder/__testUtils__/skip.ts
+++ backstage/packages/backend/src/plugins/scaffolder/__testUtils__/skip.ts
@@ -1,4 +1,5 @@
 /**
  * Skip tests on a given platform
  */
-export const skip = (platform: NodeJS.Platform) => process.platform === platform ? it.skip : it;
+export const skip = (platform: NodeJS.Platform) =>
+  process.platform === platform ? it.skip : it;
--- backstage/packages/backend/src/plugins/scaffolder/templates/project-create.test.ts
+++ backstage/packages/backend/src/plugins/scaffolder/templates/project-create.test.ts
@@ -79,31 +79,33 @@ describe('project-create: fetch:template', () => {
     `);
   });
 
-  skip('win32')('The fetch:template action should render the expected changes for the Pull Request', async () => {
-    await fetchTemplateActionHandler({
-      template: { name: 'project-create' },
-      values: {
-        requestId: '<uuid>',
-        rootFolderId: '<root-folder-id>',
-        projectName: '<project-name>',
-        projectId: '<project-id>',
-        owners: [
-          { email: 'jane.doe@gcp.hc-sc.gc.ca' },
-          { email: 'john.doe@gcp.hc-sc.gc.ca' },
-        ],
-        editors: [
-          { email: 'samantha.jones@gcp.hc-sc.gc.ca' },
-          { email: 'alex.mcdonald@gcp.hc-sc.gc.ca' },
-          { email: 'john.campbell@gcp.hc-sc.gc.ca' },
-        ],
-        costCentre: 'ABC123456789',
-        pr_targetPath: 'DMIA-PHAC/SciencePlatform/<project-name>',
-        catalogEntityOwner: 'user:default/jane.doe',
-      },
-      mockDir,
-    });
+  skip('win32')(
+    'The fetch:template action should render the expected changes for the Pull Request',
+    async () => {
+      await fetchTemplateActionHandler({
+        template: { name: 'project-create' },
+        values: {
+          requestId: '<uuid>',
+          rootFolderId: '<root-folder-id>',
+          projectName: '<project-name>',
+          projectId: '<project-id>',
+          owners: [
+            { email: 'jane.doe@gcp.hc-sc.gc.ca' },
+            { email: 'john.doe@gcp.hc-sc.gc.ca' },
+          ],
+          editors: [
+            { email: 'samantha.jones@gcp.hc-sc.gc.ca' },
+            { email: 'alex.mcdonald@gcp.hc-sc.gc.ca' },
+            { email: 'john.campbell@gcp.hc-sc.gc.ca' },
+          ],
+          costCentre: 'ABC123456789',
+          pr_targetPath: 'DMIA-PHAC/SciencePlatform/<project-name>',
+          catalogEntityOwner: 'user:default/jane.doe',
+        },
+        mockDir,
+      });
 
-    expect(mockDir.content({ path: 'workspace' })).toMatchInlineSnapshot(`
+      expect(mockDir.content({ path: 'workspace' })).toMatchInlineSnapshot(`
       {
         "catalog-info.yaml": "---
       apiVersion: backstage.io/v1alpha1
@@ -140,5 +142,6 @@ describe('project-create: fetch:template', () => {
       ",
       }
     `);
-  });
+    },
+  );
 });
