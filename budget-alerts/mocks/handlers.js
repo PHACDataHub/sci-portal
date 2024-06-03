02f2ac7b7ba0c6a41da684c0201a3b5129393589
rajan <ssubedir@gmail.com>
2024-06-03 11:26:50 -0400

    remove unused handler

--- budget-alerts/mocks/handlers.js
+++ budget-alerts/mocks/handlers.js
@@ -1,14 +1,2 @@
-const { http, graphql, HttpResponse } = require('msw');
-
-const Handlers = [
-    http.get('https://example.com/user', () => {
-        return HttpResponse.json({
-            id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
-            firstName: 'John',
-            lastName: 'Maverick',
-        })
-    }),
-];
-
-
+const Handlers = [];
 module.exports = { Handlers }
